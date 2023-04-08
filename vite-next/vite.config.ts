07582263afde7c40e5410a20937f406e0e5ab03d
CON <j2019.hkjan@gmail.com>
2023-04-09 00:37:32 +0900

    camome error

---
+++ vite-next/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { generateScopedName, hash } from "@camome/utils"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    modules: {
+      generateScopedName(name, filename) {
+        // @camome/core depends on static class names
+        // but your own module classes won't.
+        if (!filename.match(/@camome\/core/)) {
+          // Whatever.
+          return name + "-" + hash(filename);
+        }
+        return generateScopedName(name, filename);
+      },
+    },
+  },
+})
